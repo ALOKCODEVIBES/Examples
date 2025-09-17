13417ada91c13c0e53df69066c72deb26126f884
Alok RAJ <alokraj0324@gmail.com>
2025-09-17 18:29:07 +0530

    third commit

--- src/com/alokcodevibes/Main.java
+++ src/com/alokcodevibes/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("ALok Raj Kuswaha");
 		System.out.println("wELCOME TO alokcodevibes");
+		System.out.println("configure git to jenkins");
 	}
 
 }
